76b1c189536665ac5649f12ce37018b151c16e8c
yan357876460 <travel_926@163.com>
2016-08-04 11:10:42 +0800

    第二次提交整个项目

--- app/src/main/java/com/qf/yueshijia/MainActivity.java
+++ app/src/main/java/com/qf/yueshijia/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
